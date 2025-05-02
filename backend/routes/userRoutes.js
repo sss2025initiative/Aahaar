58ebb3504ef9e333438500d859c1b5a66234bb03
sss2025initiative <sss.initiative.2025@gmail.com>
2025-05-02 16:32:40 +0530

    Delete backend directory

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import { authUser, logoutUser, registerUser } from "../controllers/userController.js";
-import { protect } from "../middlewares/authMiddleware.js";
-import express from "express";
-const router = express.Router();
-
-router.route("/register").post(registerUser);
-router.post("/auth", authUser);
-router.post("/logout", logoutUser);
-export default router;
