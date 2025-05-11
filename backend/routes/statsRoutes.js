b0c18c12bab2bb3343adaa4a585403fdb8c7ac13
Santosh patel <santoshpatelvns5@gmail.com>
2025-05-11 22:25:05 +0530

    stats setup totalNgo approvedNGO , total donor , approve donor ,top donor, trend,Approval avg time

--- backend/routes/statsRoutes.js
+++ backend/routes/statsRoutes.js
@@ -1,1 +1,7 @@
-//stats routes
+import express from "express";
+import { getStats } from "../controllers/statsController.js";
+import { protect } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+router.route("/stats").get(protect, getStats);
+export default router;
