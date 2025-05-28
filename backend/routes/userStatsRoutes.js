4fbfe63ee06e59ced4e1d5ebf275b8f6e0862d26
Santosh patel <santoshpatelvns5@gmail.com>
2025-05-28 15:10:23 +0530

    User Stats

--- backend/routes/userStatsRoutes.js
+++ backend/routes/userStatsRoutes.js
@@ -1,1 +1,9 @@
 //Routes for user stats
+import express from "express";
+import { getUserDashboardStats } from "../controllers/userStatsController.js";
+
+const router = express.Router();
+
+router.get("/user-dashboard-stats/:donorId", getUserDashboardStats);
+
+export default router;
